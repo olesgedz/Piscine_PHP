422e498aa8f5c9f49165d3c60747577d33b076fb
olesgedz <olesgedz@rambler.ru>
2019-05-02 22:27:03 +0300

    something red0 ssap2 plz

---
+++ Day02/ex00/another_world.php
@@ -0,0 +1,12 @@
+#!/usr/bin/php
+<?php
+	function main($argc, $argv)
+	{
+		if ($argc < 2)
+			return (0);
+		$argv[1] = trim($argv[1]);
+		$argv[1] = preg_filter("/\s+/", " ", $argv[1]);
+		echo $argv[1]."\n";
+	}
+
+	main($argc, $argv);
