7f22d690bc781e07057efcc1fcd68a846036150e
olesgedz <olesgedz@rambler.ru>
2019-05-03 22:25:12 +0300

    ex05

--- Day02/ex04/photos.php
+++ Day02/ex04/photos.php
@@ -1,5 +1,5 @@
 #!/usr/bin/php
-<?php
+<?PHP
 	function	filter_all($site, $addr)
 	{
 		$valid = array();
@@ -33,8 +33,7 @@
 			fclose($fp);
 		}
 	}
-    
-    function 	download_all($tab, $dirname)
+	function 	download_all($tab, $dirname)
 	{
 		if (file_exists($dirname) === FALSE)
 			@mkdir($dirname);
@@ -43,56 +42,22 @@
 			$dst = $dirname.'/'.end(explode('/', $s));
 			down_img($s, $dst);
 		}
-    }
-    
-    function getFileContents($url)
-    {
-        // Workaround: Save temp file
-        $img = tempnam(".", 'pdf-');
-        $img .= '.' . pathinfo($url, PATHINFO_EXTENSION);
-    
-        $fp = fopen($img, 'w+');
-    
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_FILE, $fp);
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-    
-        $result = curl_exec($ch);
-        curl_close($ch);
-    
-        fclose($fp);
-    
-        return $result ? $img : false;
-    }
-
-    function main($argc, $argv)
-    {
-        if ($argc == 2)
-        {
-            //down_img("https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png", ".");
-            print(getFileContents("https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png"));
-            
-            // $dirname = preg_replace("#https?://#", "", $argv[1]);
-            // $dirname = array_filter(explode('/', $dirname));
-            // $dirname = $dirname[0];
-            // $cu = curl_init($argv[1]);
-            // if ($cu !== FALSE)
-            // {
-            //     curl_setopt($cu, CURLOPT_URL, $argv[1]);
-            //     curl_setopt($cu, CURLOPT_RETURNTRANSFER, 1);
-            //     if ($site = @curl_exec($cu))
-            //     {
-            //         $tab = filter_all($site, $argv[1]);
-            //         download_all($tab, $dirname);
-            //         curl_close($cu);
-            //     }
-            // }
-        }
-    }
-
-    main($argc, $argv);
+	}
+	if ($argc == 2)
+	{
+		$dirname = preg_replace("#https?://#", "", $argv[1]);
+		$dirname = array_filter(explode('/', $dirname));
+		$dirname = $dirname[0];
+		$cu = curl_init($argv[1]);
+		if ($cu !== FALSE)
+		{
+			curl_setopt($cu, CURLOPT_URL, $argv[1]);
+			curl_setopt($cu, CURLOPT_RETURNTRANSFER, 1);
+			if ($site = @curl_exec($cu))
+			{
+				$tab = filter_all($site, $argv[1]);
+				download_all($tab, $dirname);
+				curl_close($cu);
+			}
+		}
+	}
---
+++ Day02/ex05/denial.php
@@ -0,0 +1,37 @@
+#!/usr/bin/php
+<?php
+    if ($argc != 3)
+        exit();
+    if (!file_exists($argv[1]))
+        exit();
+    $read = fopen($argv[1], 'r');
+    
+    while ($read && !feof($read))
+        $array[] = explode(";", fgets($read));
+    $header = $array[0];
+    unset($array[0]);
+    foreach ($header as $k => $v)
+        $header[$k] = trim($v);
+    $index = array_search($argv[2], $header);
+    if ($index === false)
+        exit();
+    $name = array();
+    foreach ($header as $header_k => $header_v){
+        
+        foreach ($array as $v) {
+            print_r($v);
+            if (isset($v[$index]))
+                $name[trim($v[$index])] = trim($v[0]);
+        }
+        $$header_v = $name;
+    }
+    $stdin = fopen("php://stdin", "r");
+    while ($stdin && !feof($stdin)) {
+        echo "Enter your command: ";
+        $order = fgets($stdin);
+        if ($order) {
+            eval($order);
+        }
+    }
+    fclose($stdin);
+    echo "\n";
