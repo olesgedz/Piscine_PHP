8654906bf74e97c0ebff75bfd0e88c701bf6efd9
olesgedz <olesgedz@rambler.ru>
2019-05-06 16:48:40 +0300

    asd

--- Day02/ex04/photos.php
+++ Day02/ex04/photos.php
@@ -1,9 +1,10 @@
 #!/usr/bin/php
-<?PHP
+<?php
+
 	function	filter_all($site, $addr)
 	{
 		$valid = array();
-		preg_match_all("#<img.*? src\s*=\s*[\"'](.*?)[\"'].*?>?#mi", $site, $img);
+		preg_match_all("#<img.*? src\s*=\s*[\"'](.*?)[\"'].*?>?#mi", $site, $img); /*preg_match_all('/<img.*src="([^"]*)"/', $site, $img)*/
 		foreach ($img[1] as $s)
 		{
 			if (!(preg_match("#^(https?)://*#mi", $s)))
@@ -18,6 +19,7 @@
 		}
 		return ($valid);
 	}
+
 	function 	down_img($srclink, $dst)
 	{
 		$ch = curl_init($srclink);
@@ -33,31 +35,37 @@
 			fclose($fp);
 		}
 	}
+
 	function 	download_all($tab, $dirname)
 	{
 		if (file_exists($dirname) === FALSE)
 			@mkdir($dirname);
+		print_r($tab);
 		foreach ($tab as $s)
 		{
 			$dst = $dirname.'/'.end(explode('/', $s));
 			down_img($s, $dst);
 		}
 	}
-	if ($argc == 2)
+
+	function main($argc, $argv)
 	{
-		$dirname = preg_replace("#https?://#", "", $argv[1]);
-		$dirname = array_filter(explode('/', $dirname));
-		$dirname = $dirname[0];
-		$cu = curl_init($argv[1]);
-		if ($cu !== FALSE)
+		if ($argc == 2)
 		{
-			curl_setopt($cu, CURLOPT_URL, $argv[1]);
-			curl_setopt($cu, CURLOPT_RETURNTRANSFER, 1);
-			if ($site = @curl_exec($cu))
+			$dirname = preg_replace("#https?://#", "", $argv[1]);
+			$dirname = array_filter(explode('/', $dirname));
+			$dirname = $dirname[0];
+			$cu = curl_init($argv[1]);
+			if ($cu !== FALSE)
 			{
-				$tab = filter_all($site, $argv[1]);
-				download_all($tab, $dirname);
-				curl_close($cu);
+				curl_setopt($cu, CURLOPT_URL, $argv[1]);
+				curl_setopt($cu, CURLOPT_RETURNTRANSFER, 1); //
+				if ($site = curl_exec($cu))
+				{
+					$tab = filter_all($site, $argv[1]);
+					download_all($tab, $dirname);
+					curl_close($cu);
+				}
 			}
 		}
 	}
